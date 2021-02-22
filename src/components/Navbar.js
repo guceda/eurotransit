9390ebc95206cb4fc7e4313d302d389bf3ebd521
Gonzalo Uceda Castro <gon_u@hotmail.com>
2021-02-22 20:30:06 +0100

    Make navbar generic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useCallback } from "react";
 
-const Navbar = ({selectedTransport, setTransport}) => {
-    return (
-        <div className="navbar">
-            <div className="tab">
-                <button className={selectedTransport == "plane" ? "tab-active" : "tab-btn"} onClick={() => setTransport("plane")}>Flight</button>
-                <button className={selectedTransport == "train" ? "tab-active" : "tab-btn"} onClick={() => setTransport("train")}>Train</button>
-                <button className="tab-btn">About</button>
-            </div>
-        </div>
-    );
+const Navbar = ({ options, selected, setSelected }) => {
+  const setClass = useCallback(
+    (method) => {
+      return selected === method ? "tab-active" : "tab-btn";
+    },
+    [selected]
+  );
+
+  return (
+    <div className="navbar">
+      <div className="tab">
+        {options.map((opt) => (
+          <button
+            className={setClass(opt.value)}
+            onClick={() => setSelected(opt.value)}
+          >
+            {opt.label}
+          </button>
+        ))}
+        <button className="tab-btn">About</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
