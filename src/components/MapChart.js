8ffbe9227a5b458e96e132824e74b0cbf536178d
Gonzalo Uceda Castro <gon_u@hotmail.com>
2021-02-23 20:05:02 +0100

    enable zoom and panning

--- src/components/MapChart.js
+++ src/components/MapChart.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
 import geodata from "../geo.json";
 
 const MapChart = ({ dataset, scale, selectedTransport, selectedYear }) => {
-  
   const getAcc = (data) => {
     const amount = data
       ? Object.values(data).reduce((acc, v) => acc + v || 0, 0)
@@ -20,35 +19,37 @@ const MapChart = ({ dataset, scale, selectedTransport, selectedYear }) => {
           scale: 500,
         }}
       >
-        <Geographies geography={geodata}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const amount = getAcc(dataset[geo.properties.ISO_A2]);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  stroke="#EAEAEC"
-                  style={{
-                    default: {
-                      fill: scale(amount),
-                      opacity: 1,
-                      outline: "none",
-                    },
-                    hover: {
-                      fill: '#0e755d',
-                      outline: "none",
-                    },
-                    pressed: {
-                      fill: "#E42",
-                      outline: "none",
-                    },
-                  }}
-                />
-              );
-            })
-          }
-        </Geographies>
+        <ZoomableGroup>
+          <Geographies geography={geodata}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const amount = getAcc(dataset[geo.properties.ISO_A2]);
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    stroke="#EAEAEC"
+                    style={{
+                      default: {
+                        fill: scale(amount),
+                        opacity: 1,
+                        outline: "none",
+                      },
+                      hover: {
+                        fill: "#0e755d",
+                        outline: "none",
+                      },
+                      pressed: {
+                        fill: "#E42",
+                        outline: "none",
+                      },
+                    }}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        </ZoomableGroup>
       </ComposableMap>
     </div>
   );
