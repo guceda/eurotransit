796ae94df3d4ff5330fb5ce5cec4b08c7dc663a7
Gonzalo Uceda Castro <gon_u@hotmail.com>
2021-02-24 14:21:19 +0100

    consider years in scale

--- src/customHooks.js
+++ src/customHooks.js
@@ -57,14 +57,16 @@ export const useDatasetsLimits = (data) => {
   const limits = useMemo(() => {
     let max = -Infinity;
     let min = Infinity;
-    Object.values(data).forEach((country) =>
-      Object.values(country).forEach((val) => {
-        if (val) {
-          max = Math.max(val, max);
-          min = Math.min(val, min);
-        }
-      })
-    );
+    Object.values(data).forEach((year) => {
+        return Object.values(year).forEach((country) =>
+          Object.values(country).forEach((val) => {
+            if (val) {
+              max = Math.max(val, max);
+              min = Math.min(val, min);
+            }
+          })
+        );
+      });
     return [max, min];
   }, [data]);
 
