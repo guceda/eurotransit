2003bd18f3f43366a708168c88e6a702c222f608
Fabian Paier <fabianpaier@n186-p58.eduroam.kth.se>
2021-03-02 14:04:21 +0100

    Added the population and passengers per 1M inhabitants

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,9 +6,12 @@ const Sidebar = ({ countries, transport, year, dataset}) => {
   return (
     /* Sample content */
     <div style={{ backgroundColor: "lightgrey", height: "100%" }}>
+
       <h1>{countries[0] ?.geo.properties.NAME}</h1>
-      <span>{transport}</span>
-      <span>{year}</span>
+      <div>
+        <h2>Population: {countries[0]?.geo.properties.POP_EST}</h2>
+        <h2>Passengers per 1M population:</h2>
+      </div>
       <h2>Connections</h2>
       
 <StackedBarChart />
