18fa906cd1e4eac95318757639ac0532abee2470
Gonzalo Uceda Castro <gon_u@hotmail.com>
2021-03-02 11:41:26 +0100

    Sidebar:: add more sample content

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,20 @@
-
-const Sidebar = ({countries, transport, year}) => {
-
+const Sidebar = ({ countries, transport, year }) => {
+  debugger;
   return (
     /* Sample content */
-    <div style = {{backgroundColor: 'lightgrey', height: '100%'}}>
-      {countries[0]?.geo.properties.NAME}
-      <p>{transport}</p>
-      <p>{year}</p>
+    <div style={{ backgroundColor: "lightgrey", height: "100%" }}>
+      <h1>{countries[0]?.geo.properties.NAME}</h1>
+      <h3>{transport}</h3>
+      <h3>{year}</h3>
+      <h2>Connections</h2>
+      {Object.keys(countries[0].data)
+        .filter((x) => countries[0].data[x])
+        .map((target) => (
+          <div key={target}>{`${target}: ${countries[0].data[target]}`}</div>
+        ))}
     </div>
     /* -------------- */
   );
 };
 
 export default Sidebar;
-
