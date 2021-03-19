4f19d0f04fd888dfcc1ccbdfb37f0061445c5eb3
Gonzalo Uceda Castro <gon_u@hotmail.com>
2021-03-19 17:38:19 +0100

    fix legend

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -5,26 +5,30 @@ import useColorSet from "../hooks/useColorSet";
 
 import theme from "../../src/theme.json";
 
-
 const Legend = ({ limits, transportation }) => {
-    const [max, min] = limits
-    const range = [theme.map[transportation].min, theme.map[transportation].max];
-    const color_range = chroma.scale(range).domain([min, max]).colors(8);
-    return (
-        <div>
-            <div className="legend">
-                <div className="legend__colors">
-                    {color_range.map((opt) => (
-                        <div style={{ backgroundColor: opt, width: "50px", height: "20px" }}></div>
-                    ))}
-                </div>
-                <div className="legend__description">
-                    <div className="legend__text">{min} Passenger</div>
-                    <div className="legend__text">{(max/1000000).toFixed(2)}M Passengers</div>
-                </div>
-            </div>
+  const [max, min] = limits;
+  const range = [theme.map[transportation].min, theme.map[transportation].max];
+  const color_range = chroma.scale(range).domain([min, max]).colors(8);
+  return (
+    <div>
+      <div className="legend">
+        <div className="legend__colors">
+          {color_range.map((opt) => (
+            <div
+              key={opt}
+              style={{ backgroundColor: opt, width: "50px", height: "20px" }}
+            ></div>
+          ))}
         </div>
-    );
+        <div className="legend__description">
+          <div className="legend__text">{min} Passenger</div>
+          <div className="legend__text">
+            {(max / 1000000).toFixed(2)}M Passengers
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Legend;
