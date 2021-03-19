a94ab92c4eb8473599d20f9607b6012bddfa6b2e
Gonzalo Uceda Castro <gon_u@hotmail.com>
2021-03-19 13:44:23 +0100

    Fix warning

--- src/components/sidebar/StackedBarChart.jsx
+++ src/components/sidebar/StackedBarChart.jsx
@@ -1,4 +1,3 @@
-
 import { Bar } from "react-chartjs-2";
 import { YEAR_OPTS } from "../../constants";
 import { COUNTRY_COLORS } from "../../constants";
@@ -7,11 +6,9 @@ const StackedBarChart = ({ dataset, countryCode, codes, transport }) => {
   const countries = Object.keys(dataset[YEAR_OPTS[0].value]);
 
   const datasets = countries.map((country, idx) => {
-
     const years = [];
 
     YEAR_OPTS.forEach(({ value }) => {
-
       // Remove own country
       if (country === countryCode) return;
 
@@ -25,12 +22,12 @@ const StackedBarChart = ({ dataset, countryCode, codes, transport }) => {
       label: codes[country],
       data: years,
       backgroundColor: COUNTRY_COLORS[country],
-    }
+    };
   });
 
   const data = {
     labels: YEAR_OPTS.map((year) => year.value),
-    datasets: datasets
+    datasets: datasets,
   };
 
   const options = {
@@ -39,39 +36,45 @@ const StackedBarChart = ({ dataset, countryCode, codes, transport }) => {
       yAxes: [
         {
           gridLines: {
-            color: '#404040'
+            color: "#404040",
           },
           stacked: true,
           ticks: {
             callback: function (value, index, values) {
-              if((value / 1000000) >= 1) return `${value / 1000000}M`;
+              if (value / 1000000 >= 1) return `${value / 1000000}M`;
               else return `${value / 1000}K`;
             },
             beginAtZero: true,
-            fontColor: "lightgrey"
+            fontColor: "lightgrey",
           },
           scaleLabel: {
             display: false,
-            labelString: 'number of passengers',
-            fontColor: "lightgrey"
-          }
+            labelString: "number of passengers",
+            fontColor: "lightgrey",
+          },
         },
       ],
       xAxes: [
         {
           gridLines: {
-            color: '#404040'
+            color: "#404040",
           },
           stacked: true,
           ticks: {
-            fontColor: "lightgrey"
+            fontColor: "lightgrey",
           },
         },
       ],
     },
   };
 
-  return <Bar options={options} data={data} />;
+  return (
+    <Bar
+      options={options}
+      data={data}
+      datasetKeyProvider={(d) => d.key}
+    />
+  );
 };
 
 export default StackedBarChart;
